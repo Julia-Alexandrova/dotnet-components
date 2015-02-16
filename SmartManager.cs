c3506b074bd3a64ba6b446825ae0371bf9a952c3
julia-alexandrova <julalexandrova@gmail.com>
2015-02-16 19:56:49 +0300

    initial commit

---
+++ SmartManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lib
+{
+  public class SmartManager
+  {
+    public void DoSmsVerySmart()
+    {
+      if (Environment.Is64BitOperatingSystem)
+        DoSmsOn64BitOS();
+      else
+        DoSmsOn32BitOS();
+    }
+
+    private void DoSmsOn64BitOS()
+    {
+      while (true)
+      {
+        Console.Out.WriteLine("Sms goes wrong...");
+      }
+    }
+
+    private void DoSmsOn32BitOS()
+    {
+      Console.Out.WriteLine("No no no! It's ok here...");
+    }
+  }
+}
